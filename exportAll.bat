4b9913a2494a08b75e8125317e2c2861c85a25d9
Stephanie Barrett <stephaniebarrett@live.ca>
2023-08-08 20:34:04 -0400

    windows script to output all .stl models

---
+++ exportAll.bat
@@ -0,0 +1,15 @@
+@echo off
+cls
+SETLOCAL EnableDelayedExpansion
+FOR /l %%G IN (1,1,6) DO (
+	set filename=rack%%G.stl
+	start /B openscad -o !filename! -D DRAW_RACK=%%G main.scad
+)
+FOR /l %%G IN (1,1,3) DO (
+	set filename=joinery%%G.stl
+	start /B openscad -o !filename! -D DRAW_JOINERY=%%G main.scad
+)
+FOR /l %%G IN (1,1,2) DO (
+	set filename=hddcage%%G.stl
+	start /B openscad -o !filename! -D DRAW_HDD_CAGE=%%G main.scad
+)
